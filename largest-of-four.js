251b11108295f1c7300beaee5696e01cafcb064b
Flatiron School <dnick13@gmail.com>
2017-01-04 11:20:32 -0500

    Initial commit

--- largest-of-four.js
+++ largest-of-four.js
@@ -7,6 +7,6 @@ function largestOfFour(arr) {
 
   return largestArr
 }
-
+ 
 
 console.log("Expected:: [27,5,39,1001] Got:: " + largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
